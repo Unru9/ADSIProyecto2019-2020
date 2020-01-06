b89277fc1d06f8f521418fbb0225c76082d80ddd
JulenP <julencrossunited@gmail.com>
2020-01-06 19:02:21 +0100

    Base de datos

--- BattleShip/battleship.sql
+++ BattleShip/battleship.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-01-2020 a las 23:19:15
+-- Tiempo de generación: 06-01-2020 a las 19:00:40
 -- Versión del servidor: 5.7.28-0ubuntu0.18.04.4
 -- Versión de PHP: 7.2.3
 
@@ -25,6 +25,32 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `Armamento`
+--
+
+CREATE TABLE `Armamento` (
+  `escudos` int(45) NOT NULL,
+  `misiles` int(45) NOT NULL,
+  `misilesBOOM` int(45) NOT NULL,
+  `misilesNS` int(45) NOT NULL,
+  `misilesEO` int(45) NOT NULL,
+  `emailUsuario` varchar(45) NOT NULL,
+  `precio` int(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Nivel`
+--
+
+CREATE TABLE `Nivel` (
+  `nivel` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `Partida`
 --
 
@@ -35,15 +61,57 @@ CREATE TABLE `Partida` (
   `puntos` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Usuario`
+--
+
+CREATE TABLE `Usuario` (
+  `nombreUsuario` varchar(45) NOT NULL,
+  `contrasena` varchar(45) NOT NULL,
+  `esAdministrador` tinyint(1) NOT NULL,
+  `emailusuario` varchar(45) NOT NULL,
+  `puntos` int(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `Armamento`
+--
+ALTER TABLE `Armamento`
+  ADD PRIMARY KEY (`emailUsuario`);
+
+--
+-- Indices de la tabla `Nivel`
+--
+ALTER TABLE `Nivel`
+  ADD PRIMARY KEY (`nivel`);
 
 --
 -- Indices de la tabla `Partida`
 --
 ALTER TABLE `Partida`
   ADD PRIMARY KEY (`nombreUsuario`,`nivel`,`fechaHora`);
+
+--
+-- Indices de la tabla `Usuario`
+--
+ALTER TABLE `Usuario`
+  ADD PRIMARY KEY (`nombreUsuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `Nivel`
+--
+ALTER TABLE `Nivel`
+  MODIFY `nivel` int(100) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
