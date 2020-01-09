b0200cc0b51e480839dc0703b79d6905b2128555
Unru9 <44617781+Unru9@users.noreply.github.com>
2020-01-09 17:24:30 +0100

    Add files via upload

--- BattleShip/src/packVista/VentanaJuegoPerdedor.java
+++ BattleShip/src/packVista/VentanaJuegoPerdedor.java
@@ -15,13 +15,17 @@ import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 import javax.swing.border.EmptyBorder;
 
-import packCompartirResultados.TwitterJ;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import packGestores.GCompartirResultadosRRSS;
+import packModelo.Battleship;
+
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 
 public class VentanaJuegoPerdedor extends JFrame {
@@ -49,21 +53,21 @@ public class VentanaJuegoPerdedor extends JFrame {
 	 */
 	public VentanaJuegoPerdedor() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(360, 250, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 
 		setTitle("Compartir Resultados");
-		setSize(400, 200);
+		setSize(600, 200);
 		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
 		//TÍTULO PRINCIPAL
 		JLabel título = new JLabel(" ¡INTÉNTALO OTRA VEZ, HAS PERDIDO ! ");
-		título.setBounds(100, 23, 250, 14);
+		título.setBounds(190, 23, 250, 14);
 		
 		//COMPARTIR TWITTER
-		ImageIcon TwitterButtonIcon = new ImageIcon("./images/data/twitter.png");
+		ImageIcon TwitterButtonIcon = new ImageIcon(VentanaJuegoPerdedor.class.getResource("/packImages/twitter.png"));
 		Image img1 = TwitterButtonIcon.getImage();
 		Image img2= img1.getScaledInstance(40, 40, Image.SCALE_SMOOTH);
 		ImageIcon x= new ImageIcon(img2);
@@ -72,17 +76,22 @@ public class VentanaJuegoPerdedor extends JFrame {
 		
 		TwitterButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				TwitterJ twitter = TwitterJ.getTwitter();
+				GCompartirResultadosRRSS RRSS=GCompartirResultadosRRSS.getcompartirResultadosRRSS();
+				Battleship BattleShip= Battleship.getBattleship();
+				JSONObject res;
 				try {
-					twitter.escribirTweet();
-					Desktop.getDesktop().browse(new URI("https://www.twitter.com/adsi2019"));
-				} catch (TwitterException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
+					res=BattleShip.obtDatos();
+					RRSS.compartirTwitter(res.getString("nombreUsuario"), res.getInt("puntos"),150);
 				} catch (IOException e2) {
 					// TODO Auto-generated catch block
 					e2.printStackTrace();
 				} catch (URISyntaxException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (JSONException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (SQLException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
@@ -93,27 +102,86 @@ public class VentanaJuegoPerdedor extends JFrame {
 		TwitterButton.setHorizontalTextPosition(SwingConstants.RIGHT);
 		
 		
-		//COMPARTIR FACEBOOK
-		ImageIcon FacebookButtonIcon = new ImageIcon("./images/data/facebook.png");
-		Image img3 = FacebookButtonIcon.getImage();
+		//COMPARTIR WHATSAPPS
+		ImageIcon WhastAppButtonIcon = new ImageIcon(VentanaJuegoPerdedor.class.getResource("/packImages/whatsapp.png"));
+		Image img3 = WhastAppButtonIcon.getImage();
 		Image img4= img3.getScaledInstance(40, 40, Image.SCALE_SMOOTH);
 		ImageIcon y= new ImageIcon(img4);	
-		JButton FacebookButton = new JButton("Facebook", y);
+		JButton WhatsAppButton = new JButton("WhatsApp", y);
 		
-		FacebookButton.addActionListener(new ActionListener() {
+		WhatsAppButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				GCompartirResultadosRRSS RRSS=GCompartirResultadosRRSS.getcompartirResultadosRRSS();
+				Battleship BattleShip= Battleship.getBattleship();
+				JSONObject res;
+				try {
+					res=BattleShip.obtDatos();
+					RRSS.compartirWhatsApp(res.getString("nombreUsuario"), res.getInt("puntos"));
+				} catch (IOException e2) {
+					// TODO Auto-generated catch block
+					e2.printStackTrace();
+				} catch (URISyntaxException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (JSONException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 				
 			}
 		});
-		FacebookButton.setBounds(224, 63, 150, 40);
-		FacebookButton.setHorizontalTextPosition(SwingConstants.RIGHT);
+		WhatsAppButton.setBounds(224, 63, 150, 40);
+		WhatsAppButton.setHorizontalTextPosition(SwingConstants.RIGHT);
 		
+		contentPane.setLayout(null);
+		contentPane.setLayout(null);
+		
+		
+		
+		//COMPARTIR REDDIT
+		ImageIcon RedditButtonIcon = new ImageIcon(VentanaJuegoPerdedor.class.getResource("/packImages/reddit.png"));
+		Image img5 = RedditButtonIcon.getImage();
+		Image img6= img5.getScaledInstance(40, 40, Image.SCALE_SMOOTH);
+		ImageIcon z= new ImageIcon(img6);	
+		JButton RedditButton = new JButton("Reddit", z);
+				
+		RedditButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				GCompartirResultadosRRSS RRSS=GCompartirResultadosRRSS.getcompartirResultadosRRSS();
+				Battleship BattleShip= Battleship.getBattleship();
+				JSONObject res;
+				try {
+					res=BattleShip.obtDatos();
+					RRSS.compartirReddit(res.getString("nombreUsuario"), res.getInt("puntos"));
+				} catch (IOException e2) {
+					// TODO Auto-generated catch block
+					e2.printStackTrace();
+				} catch (URISyntaxException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (JSONException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				
+			}
+		});
+		RedditButton.setBounds(401, 63, 150, 40);
+		RedditButton.setHorizontalTextPosition(SwingConstants.RIGHT);
+				
 		contentPane.setLayout(null);
 		contentPane.setLayout(null);
 
 		getContentPane().add(título);
 		getContentPane().add(TwitterButton);
-		getContentPane().add(FacebookButton);
+		getContentPane().add(WhatsAppButton);
+		getContentPane().add(RedditButton);
 		
 	}
 }
