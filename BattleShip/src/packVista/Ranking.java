74ae77f9c2334163a7523438cb44441ad681ddfe
JulenP <julencrossunited@gmail.com>
2020-01-10 15:45:12 +0100

    Base de datos

--- BattleShip/src/packVista/Ranking.java
+++ BattleShip/src/packVista/Ranking.java
@@ -6,10 +6,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JTextArea;
+import javax.swing.JScrollBar;
+import javax.swing.JTable;
 
 public class Ranking extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -38,6 +43,31 @@ public class Ranking extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		DefaultTableModel model = new DefaultTableModel();
+		model.addColumn("nombre");
+		table = new JTable(model);
+		contentPane.add(table, BorderLayout.CENTER);
+		System.out.println(string);
+		switch(string) {
+		case "PersonalGeneral":
+			cargarPersonaGlobal(model);
+			break;
+		case "PersonalPor Niveles":
+			break;
+		case "GlobalGeneral":
+			break;
+		case "GlobalPor Niveles":
+			break;
+		}
+			
+	}
+
+	private void cargarPersonaGlobal(DefaultTableModel model) {
+		// TODO Auto-generated method stub
+		System.out.println("entra");
+		String[] headers= {"nombre", "puntos"};
+		String[][] data= {{"test","1"},{"test","2"}};
+		
 	}
 
 }
