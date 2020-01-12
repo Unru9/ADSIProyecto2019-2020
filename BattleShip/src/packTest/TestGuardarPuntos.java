cd77634f194fe86a8d3c0f673e1a5a12345baa79
Lerulolu <46162886+Lerulolu@users.noreply.github.com>
2020-01-12 17:49:20 +0100

    .

--- BattleShip/src/packTest/TestGuardarPuntos.java
+++ BattleShip/src/packTest/TestGuardarPuntos.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package packTest;
 
 import static org.junit.Assert.*;
@@ -10,8 +9,6 @@ import java.text.SimpleDateFormat;
 import org.junit.Test;
 
 import packGestores.SGBD;
-import packModelo.Battleship;
-import packModelo.packJugador.Usuario;
 
 public class TestGuardarPuntos {
 
@@ -27,61 +24,6 @@ public class TestGuardarPuntos {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		Usuario us = new Usuario();
-		us.setNombreUsuario("test");
-		Battleship.getBattleship().setUsuario(us);
-		Battleship.getBattleship().guardarPuntos();
-		try {
-			SGBD.getSGBD().execSQL("DELETE FROM Partida WHERE nombreUsuario='test';");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
 	}
 
 }
-=======
-package packTest;
-
-import static org.junit.Assert.*;
-
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-
-import org.junit.Test;
-
-import packGestores.SGBD;
-import packModelo.Battleship;
-import packModelo.packJugador.Usuario;
-
-public class TestGuardarPuntos {
-
-	@Test
-	public void test() {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		Timestamp timestamp=new Timestamp(System.currentTimeMillis());
-		String ts = sdf.format(timestamp);
-		try {
-			SGBD.getSGBD().execSQL("INSERT INTO Partida VALUES('test','facil','"+ts+"',100);");
-			SGBD.getSGBD().execSQL("DELETE FROM Partida WHERE nombreUsuario='test';");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		Usuario us = new Usuario();
-		us.setNombreUsuario("test");
-		Battleship.getBattleship().setUsuario(us);
-		Battleship.getBattleship().guardarPuntos();
-		try {
-			SGBD.getSGBD().execSQL("DELETE FROM Partida WHERE nombreUsuario='test';");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-	}
-
-}
->>>>>>> e08a51fd2c4dbb1a506f44411b294982193443c7
