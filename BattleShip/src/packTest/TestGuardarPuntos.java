017e6af9392a3f227e1b68781c54944b0bd38733
Lerulolu <46162886+Lerulolu@users.noreply.github.com>
2020-01-12 13:19:29 +0100

    Cambios

--- BattleShip/src/packTest/TestGuardarPuntos.java
+++ BattleShip/src/packTest/TestGuardarPuntos.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package packTest;
 
 import static org.junit.Assert.*;
@@ -28,34 +27,3 @@ public class TestGuardarPuntos {
 	}
 
 }
-=======
-package packTest;
-
-import static org.junit.Assert.*;
-
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-
-import org.junit.Test;
-
-import packGestores.SGBD;
-
-public class TestGuardarPuntos {
-
-	@Test
-	public void test() {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		Timestamp timestamp=new Timestamp(System.currentTimeMillis());
-		String ts = sdf.format(timestamp);
-		try {
-			SGBD.getSGBD().execSQL("INSERT INTO Partida VALUES('test','facil','"+ts+"',100);");
-			SGBD.getSGBD().execSQL("DELETE FROM Partida WHERE nombreUsuario='test';");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/Unru9/ADSIProyecto2019-2020.git
