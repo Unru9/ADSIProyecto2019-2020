0c3cf8edb425c2235b3f128619532bcb90c66832
Lerulolu <46162886+Lerulolu@users.noreply.github.com>
2020-01-12 18:44:00 +0100

    Test de tienda

---
+++ BattleShip/src/packTest/TiendaTest.java
@@ -0,0 +1,91 @@
+package packTest;
+
+import static org.junit.Assert.*;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import packGestores.SGBD;
+
+public class TiendaTest {
+
+	@Test
+	public void getPuntosUsuario() {
+		try {
+			ResultSet rs = SGBD.getSGBD().execSQLSelect("SELEC `puntos` FROM Partida WHERE nombreUsuario = 'Unai'");
+			int puntos = rs.getInt("puntos");
+			assertEquals(puntos, 5);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	
+	@Test
+	public void getEscudos() {
+		try {
+			ResultSet rs = SGBD.getSGBD().execSQLSelect("SELECT `puntos`, `cuantos`, `nombreArma` FROM partida NATURAL JOIN armamentoJugador WHERE nombreJugador = 'Unai' AND nombreArma = 'escudos'");
+			int armas = rs.getInt("cuantos");
+			System.out.println(armas);
+			assertEquals(armas, 14);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void getMisiles() {
+		try {
+			ResultSet rs = SGBD.getSGBD().execSQLSelect("SELECT `puntos`, `cuantos`, `nombreArma` FROM partida NATURAL JOIN armamentoJugador WHERE nombreJugador = 'Unai' AND nombreArma = 'misiles'");
+			int armas = rs.getInt("cuantos");
+			System.out.println(armas);
+			assertEquals(armas, 17);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+
+	@Test
+	public void getMisilesEO() {
+		try {
+			ResultSet rs = SGBD.getSGBD().execSQLSelect("SELECT `puntos`, `cuantos`, `nombreArma` FROM partida NATURAL JOIN armamentoJugador WHERE nombreJugador = 'Unai' AND nombreArma = 'misilesEO'");
+			int armas = rs.getInt("cuantos");
+			System.out.println(armas);
+			assertEquals(armas, 9);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void getMisilesNS() {
+		try {
+			ResultSet rs = SGBD.getSGBD().execSQLSelect("SELECT `puntos`, `cuantos`, `nombreArma` FROM partida NATURAL JOIN armamentoJugador WHERE nombreJugador = 'Unai' AND nombreArma = 'misilesNS'");
+			int armas = rs.getInt("cuantos");
+			System.out.println(armas);
+			assertEquals(armas, 8);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void getMisilesBOOM() {
+		try {
+			ResultSet rs = SGBD.getSGBD().execSQLSelect("SELECT `puntos`, `cuantos`, `nombreArma` FROM partida NATURAL JOIN armamentoJugador WHERE nombreJugador = 'Unai' AND nombreArma = 'misilesBOOM'");
+			int armas = rs.getInt("cuantos");
+			System.out.println(armas);
+			assertEquals(armas, 9);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
